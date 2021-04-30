cb643f93bae97969c3ecd14a924acae6b6c42596
Fuadi Fitri Hantoro <fuadi.hantoro@students.amikom.ac.id>
2021-04-30 14:37:37 +0700

    Update email.blade.php

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -4,12 +4,8 @@
     Selamat anda telah terdaftar di Website IP2SR
     sekarang anda sudah bisa login di website & aplikasi mobile sebagai member wifi. 
     silahkan Login ke website kemudian lengkapi profil anda.
-@component('mail::button',['url'  => 'https://ip2sr.site/login'])
-    Go to login
-@endcomponent
-@component('mail::button',['url'  => 'https://bit.ly/Apps_download'])
-    Go to download Apps
-@endcomponent
+
+    https://ip2sr.site/login
 
 Thanks,<br>
 {{config('app.name')}}
