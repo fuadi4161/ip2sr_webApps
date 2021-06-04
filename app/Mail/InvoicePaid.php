65b0223412f06ded2c31955cb7f7f6c9e9f61c38
Fuadi Fitri Hantoro <fuadi.hantoro@students.amikom.ac.id>
2021-06-04 08:47:31 +0700

    gg

--- app/Mail/InvoicePaid.php
+++ app/Mail/InvoicePaid.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class InvoicePaid extends Notification
+class InvoicePaid extends Mailable
 {
     use Queueable, SerializesModels;
 
--- app/Mail/NotifBonus.php
+++ app/Mail/NotifBonus.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class NotifBonus extends Notification
+class NotifBonus extends Mailable
 {
     use Queueable, SerializesModels;
 
